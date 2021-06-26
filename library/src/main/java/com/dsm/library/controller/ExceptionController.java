959c1e0e8ffc4ba60d5e0a776c0eb57b31dde8a3
김범진 <sdpthf@naver.com>
2021-06-26 13:03:29 +0900

    [ADD] Exception

---
+++ library/src/main/java/com/dsm/library/controller/ExceptionController.java
@@ -0,0 +1,17 @@
+package com.dsm.library.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(value = Exception.class)
+    public Map<String, String> handleException(Exception e) {
+        Map<String, String> map = new HashMap<>();
+        map.put("Error Message!", e.getMessage());
+        return map;
+    }
+}
