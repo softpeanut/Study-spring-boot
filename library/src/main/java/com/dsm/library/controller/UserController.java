236529877cf8fa68251c0f3527fefbe243aa38cd
김범진 <sdpthf@naver.com>
2021-07-14 14:23:59 +0900

    UserController

--- library/src/main/java/com/dsm/library/controller/UserController.java
+++ library/src/main/java/com/dsm/library/controller/UserController.java
@@ -3,10 +3,7 @@ package com.dsm.library.controller;
 import com.dsm.library.controller.request.UserRequest;
 import com.dsm.library.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +12,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/sign-up")
-    public String SignUp(@RequestBody UserRequest request) {
+    public String signUp(@RequestBody UserRequest request) {
         return userService.register(request);
-
     }
 
 }
