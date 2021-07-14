b05cc2e8987777a3164358e13433819c05b81cf9
김범진 <sdpthf@naver.com>
2021-07-14 23:24:36 +0900

    [MODIFY] name

--- Board/src/main/java/com/dms/project/service/delete/BoardDeletionService.java
+++ Board/src/main/java/com/dms/project/service/delete/BoardDeletionService.java
@@ -2,6 +2,6 @@ package com.dms.project.service.delete;
 
 import com.dms.project.payload.response.BoardWriteResponse;
 
-public interface BoardDeleteService {
+public interface BoardDeletionService {
     BoardWriteResponse deleteBoard(Integer id);
 }
--- Board/src/main/java/com/dms/project/service/modify/BoardModificationService.java
+++ Board/src/main/java/com/dms/project/service/modify/BoardModificationService.java
@@ -3,6 +3,6 @@ package com.dms.project.service.modify;
 import com.dms.project.payload.request.BoardRequest;
 import com.dms.project.payload.response.BoardWriteResponse;
 
-public interface BoardModifyService {
+public interface BoardModificationService {
     BoardWriteResponse modifyBoard(BoardRequest request, Integer id);
 }
