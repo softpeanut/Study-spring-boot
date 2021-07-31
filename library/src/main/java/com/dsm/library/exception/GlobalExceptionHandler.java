f1fa81d66f63d57704e0fbc78f687908c45da39a
김범진 <sdpthf@naver.com>
2021-07-31 18:13:50 +0900

    Change Exception

--- library/src/main/java/com/dsm/library/exception/GlobalExceptionHandler.java
+++ library/src/main/java/com/dsm/library/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package com.dsm.library.controller;
+package com.dsm.library.exception;
 
-import com.dsm.library.exception.BusinessException;
 import com.dsm.library.exception.response.CommonExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionController {
-
-//    @ExceptionHandler(Exception.class)
-//    public Map<String, String> handleException(Exception e) {
-//        Map<String, String> map = new HashMap<>();
-//        map.put("Error!", e.getMessage());
-//        return map;
-//    }
-
+public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<CommonExceptionResponse> handleLibraryNotFoundException(BusinessException e) {
         return new ResponseEntity<>(new CommonExceptionResponse(e.getCode(), e.getMessage()), HttpStatus.NOT_FOUND);
