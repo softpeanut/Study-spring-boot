bd25a018f855828100f034a1f14d1c3f44325d8e
김범진 <sdpthf@naver.com>
2021-07-31 21:56:17 +0900

    [Update] BookSearchService

--- library/src/main/java/com/dsm/library/service/BookSearchService.java
+++ library/src/main/java/com/dsm/library/service/BookSearchService.java
@@ -1,5 +1,6 @@
 package com.dsm.library.service;
 
+import com.dsm.library.controller.response.BookResponse;
 import com.dsm.library.domain.book.Book;
 import com.dsm.library.domain.book.BookRepository;
 import com.dsm.library.domain.library.LibraryRepository;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,13 +17,28 @@ public class BookSearchService {
     private final BookRepository bookRepository;
     private final LibraryRepository libraryRepository;
 
-    public List<Book> searchBooks() {
-        return bookRepository.findAll();
+    public List<BookResponse.BookInformation> searchBooks() {
+        List<BookResponse.BookInformation> books = bookRepository.findAll()
+                .stream()
+                .map(book -> new BookResponse.BookInformation(book.getBookId(), book.getTitle(), book.getLibrary().getName()))
+                .collect(Collectors.toList());
+
+        return books;
     }
 
-    public List<Book> searchBook(Long id) {
-        return libraryRepository.findById(id)
+    public List<BookResponse.BookInformation> searchBooksInLibrary (Long libraryId) {
+        List<BookResponse.BookInformation> bookInformations = showBooks(libraryId)
+                .stream()
+                .map(book -> new BookResponse.BookInformation(book.getBookId(), book.getTitle(), book.getLibrary().getName()))
+                .collect(Collectors.toList());
+
+        return bookInformations;
+    }
+
+    public List<Book> showBooks(Long libraryId) {
+        List<Book> books = libraryRepository.findById(libraryId)
                 .map(bookRepository::findAllByLibrary)
-                .orElseThrow(() -> new LibraryNotFoundException(id));
+                .orElseThrow(() -> new LibraryNotFoundException(libraryId));
+        return books;
     }
 }
