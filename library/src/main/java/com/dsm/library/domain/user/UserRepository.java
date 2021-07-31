0419c4cfe203af8729c8d6300a3e2223242f0082
김범진 <sdpthf@naver.com>
2021-07-31 18:13:06 +0900

    [Delete] User

--- library/src/main/java/com/dsm/library/domain/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dsm.library.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- library/src/main/java/com/dsm/library/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.dsm.library.service;
-
-import com.dsm.library.controller.request.UserRequest;
-
-public interface UserService {
-    String register(UserRequest request);
-}
