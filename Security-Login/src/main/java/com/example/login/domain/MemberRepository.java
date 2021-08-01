0276d43f8f289c583dbc2e7bd8523c6dacf4bb45
김범진 <sdpthf@naver.com>
2021-08-01 18:50:20 +0900

    MemberRepository

---
+++ Security-Login/src/main/java/com/example/login/domain/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.login.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
