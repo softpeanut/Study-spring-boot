4f55c021377a33e48cd0885c625fddf2ea191ae8
김범진 <sdpthf@naver.com>
2021-08-01 18:48:20 +0900

    MemberResponseDto

---
+++ Security-Login/src/main/java/com/example/login/controller/dto/MemberResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.login.controller.dto;
+
+import com.example.login.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberResponseDto {
+    private String email;
+
+    public static MemberResponseDto of(Member member) {
+        return new MemberResponseDto(member.getEmail());
+    }
+}
