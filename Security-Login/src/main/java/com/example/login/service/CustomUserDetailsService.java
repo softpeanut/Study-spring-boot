ca07d7c5526e405f64e387f7fdab869c2d7c1c57
김범진 <sdpthf@naver.com>
2021-08-02 21:09:58 +0900

    [ADD] CustomUserDetailsService

---
+++ Security-Login/src/main/java/com/example/login/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.login.service;
+
+import com.example.login.security.CustomUserDetails;
+import com.example.login.domain.Member;
+import com.example.login.domain.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> {
+                    return new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다. : "+ email);
+                });
+
+        return new CustomUserDetails(member);
+    }
+}
