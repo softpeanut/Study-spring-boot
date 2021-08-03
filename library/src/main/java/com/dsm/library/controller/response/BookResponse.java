e80fb19edd4d6962d6280a56b1da3421b7aa0912
김범진 <sdpthf@naver.com>
2021-08-03 14:47:25 +0900

    BookResponse

--- library/src/main/java/com/dsm/library/controller/response/BookResponse.java
+++ library/src/main/java/com/dsm/library/controller/response/BookResponse.java
@@ -3,7 +3,6 @@ package com.dsm.library.controller.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @AllArgsConstructor
