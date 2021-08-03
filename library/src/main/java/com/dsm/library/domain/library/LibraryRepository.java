628e8a844ca94958321a4ff7c15e781833a19f6c
김범진 <sdpthf@naver.com>
2021-08-03 15:30:24 +0900

    LibraryRepository

--- library/src/main/java/com/dsm/library/domain/library/LibraryRepository.java
+++ library/src/main/java/com/dsm/library/domain/library/LibraryRepository.java
@@ -3,4 +3,5 @@ package com.dsm.library.domain.library;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LibraryRepository extends JpaRepository<Library, Long> {
+    boolean existsByName(String name);
 }
