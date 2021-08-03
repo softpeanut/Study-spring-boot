f3b89a0df39b53a8f8bedf8a0438aaac547eed55
김범진 <sdpthf@naver.com>
2021-08-03 18:58:20 +0900

    ModificationBookRequest

--- library/src/main/java/com/dsm/library/controller/request/ModificationBookRequest.java
+++ library/src/main/java/com/dsm/library/controller/request/ModificationBookRequest.java
@@ -1,6 +1,5 @@
 package com.dsm.library.controller.request;
 
-import com.dsm.library.domain.library.Library;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
