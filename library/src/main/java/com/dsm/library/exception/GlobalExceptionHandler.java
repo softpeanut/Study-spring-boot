ec6fd0a2f452b13fe8453734fb20a1d438dcb2fc
김범진 <sdpthf@naver.com>
2021-08-03 15:33:50 +0900

    GlobalExceptionHandler

--- library/src/main/java/com/dsm/library/exception/GlobalExceptionHandler.java
+++ library/src/main/java/com/dsm/library/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.dsm.library.exception;
 
 import com.dsm.library.exception.response.CommonExceptionResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<CommonExceptionResponse> handleLibraryNotFoundException(BusinessException e) {
-        return new ResponseEntity<>(new CommonExceptionResponse(e.getCode(), e.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new CommonExceptionResponse(e.getCode(), e.getMessage()), e.getStatus());
     }
 }
