f8bd0c7f7c4c32dc43f8b741924d97de7094fa79
김범진 <sdpthf@naver.com>
2021-08-04 17:31:34 +0900

    MemberResponseDto

---
+++ jwt-security/src/main/java/com/example/jwtsecurity/controller/dto/MemberResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.jwtsecurity.controller.dto;
+
+import com.example.jwtsecurity.domain.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberResponseDto {
+    private String email;
+
+    public static MemberResponseDto of(Member member) {
+        return new MemberResponseDto(member.getEmail());
+    }
+
+}
