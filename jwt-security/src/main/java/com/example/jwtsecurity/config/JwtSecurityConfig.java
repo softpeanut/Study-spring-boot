51b738f8661459fdf3db022259439fb476ded8d6
김범진 <sdpthf@naver.com>
2021-08-04 17:31:01 +0900

    JwtSecurityConfig

---
+++ jwt-security/src/main/java/com/example/jwtsecurity/config/JwtSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.jwtsecurity.config;
+
+import com.example.jwtsecurity.jwt.JwtFilter;
+import com.example.jwtsecurity.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
