22a2c264e15b5642734f978b3d1a3f67a632a152
김범진 <sdpthf@naver.com>
2021-08-04 17:31:23 +0900

    MemberRequestDto

---
+++ jwt-security/src/main/java/com/example/jwtsecurity/controller/dto/MemberRequestDto.java
@@ -0,0 +1,27 @@
+package com.example.jwtsecurity.controller.dto;
+
+import com.example.jwtsecurity.domain.entity.Authority;
+import com.example.jwtsecurity.domain.entity.Member;
+import lombok.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberRequestDto {
+    private String email;
+    private String password;
+
+    public Member toMember(PasswordEncoder passwordEncoder) {
+        return Member.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .authority(Authority.ROLE_USER)
+                .build();
+    }
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, password);
+    }
+}
