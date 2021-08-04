4527dc7f9321761df5db49cd9c38042553a55cda
김범진 <sdpthf@naver.com>
2021-08-04 17:37:03 +0900

    MemberService

---
+++ jwt-security/src/main/java/com/example/jwtsecurity/service/MemberService.java
@@ -0,0 +1,28 @@
+package com.example.jwtsecurity.service;
+
+import com.example.jwtsecurity.controller.dto.MemberResponseDto;
+import com.example.jwtsecurity.domain.repository.MemberRepository;
+import com.example.jwtsecurity.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public MemberResponseDto getMemberInfo(String email) {
+        return memberRepository.findByEmail(email)
+                .map(MemberResponseDto::of)
+                .orElseThrow(() -> new RuntimeException("유저 정보가 없습니다."));
+    }
+
+    @Transactional(readOnly = true)
+    public MemberResponseDto getMyInfo() {
+        return memberRepository.findById(SecurityUtil.getCurrentMemberId())
+                .map(MemberResponseDto::of)
+                .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+    }
+}
