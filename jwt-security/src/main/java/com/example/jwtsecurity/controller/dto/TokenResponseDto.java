308ba753bf97ac8d1ef78f5b87636632c4807251
김범진 <sdpthf@naver.com>
2021-08-05 14:09:17 +0900

    Change name

--- jwt-security/src/main/java/com/example/jwtsecurity/controller/dto/TokenResponseDto.java
+++ jwt-security/src/main/java/com/example/jwtsecurity/controller/dto/TokenResponseDto.java
@@ -6,7 +6,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class TokenDto {
+public class TokenResponseDto {
     private String grantType;
     private String accessToken;
     private String refreshToken;
