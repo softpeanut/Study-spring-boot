5d97e8cea7a5cdd38acb75dd7ee964c20fb6075c
김범진 <sdpthf@naver.com>
2021-08-05 14:11:13 +0900

    MemberService

--- jwt-security/src/main/java/com/example/jwtsecurity/service/MemberService.java
+++ jwt-security/src/main/java/com/example/jwtsecurity/service/MemberService.java
@@ -13,16 +13,16 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional(readOnly = true)
-    public MemberResponseDto getMemberInfo(String email) {
-        return memberRepository.findByEmail(email)
-                .map(MemberResponseDto::of)
-                .orElseThrow(() -> new RuntimeException("유저 정보가 없습니다."));
-    }
-
-    @Transactional(readOnly = true)
     public MemberResponseDto getMyInfo() {
         return memberRepository.findById(SecurityUtil.getCurrentMemberId())
                 .map(MemberResponseDto::of)
                 .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+    }
+
+    @Transactional(readOnly = true)
+    public MemberResponseDto getMemberInfo(String email) {
+        return memberRepository.findByEmail(email)
+                .map(MemberResponseDto::of)
+                .orElseThrow(() -> new RuntimeException("유저 정보가 없습니다."));
     }
 }
