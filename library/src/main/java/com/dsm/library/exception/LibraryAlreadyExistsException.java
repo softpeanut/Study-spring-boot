97cd9304f0d91162b4559666b7455b9e0327aa8d
김범진 <sdpthf@naver.com>
2021-08-06 14:31:17 +0900

    LibraryAlreadyExistsException

--- library/src/main/java/com/dsm/library/exception/LibraryAlreadyExistsException.java
+++ library/src/main/java/com/dsm/library/exception/LibraryAlreadyExistsException.java
@@ -3,7 +3,7 @@ package com.dsm.library.exception;
 import org.springframework.http.HttpStatus;
 
 public class LibraryAlreadyExistsException extends BusinessException {
-    public LibraryAlreadyExistsException(String name) {
-        super("LIBRARY_ALREADY_EXISTS", "LibraryName이 "+name+"인 도서관이 이미 존재합니다.", HttpStatus.CONFLICT);
+    public LibraryAlreadyExistsException() {
+        super("LIBRARY_ALREADY_EXISTS", HttpStatus.CONFLICT);
     }
 }
