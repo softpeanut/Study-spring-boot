d794b88dd60650f022c2e496aac3d54c55ca2a0e
김범진 <sdpthf@naver.com>
2021-08-06 14:31:28 +0900

    LibraryNotFoundException

--- library/src/main/java/com/dsm/library/exception/LibraryNotFoundException.java
+++ library/src/main/java/com/dsm/library/exception/LibraryNotFoundException.java
@@ -3,10 +3,7 @@ package com.dsm.library.exception;
 import org.springframework.http.HttpStatus;
 
 public class LibraryNotFoundException extends BusinessException {
-    public LibraryNotFoundException(long libraryId) {
-        super("LIBRARY_NOT_FOUND", "LibraryId가 " + libraryId + "인 도서관을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
-    }
     public LibraryNotFoundException() {
-        super("LIBRARY_NOT_FOUND", "Library 없음.", HttpStatus.NOT_FOUND);
+        super("LIBRARY_NOT_FOUND", HttpStatus.NOT_FOUND);
     }
 }
