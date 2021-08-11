7e22a4ea03536040ed34cff0657abbf0fee81ef2
김범진 <sdpthf@naver.com>
2021-08-11 19:52:26 +0900

    AuthController

--- jwt-security/src/main/java/com/example/jwtsecurity/controller/AuthController.java
+++ jwt-security/src/main/java/com/example/jwtsecurity/controller/AuthController.java
@@ -7,10 +7,7 @@ import com.example.jwtsecurity.controller.dto.TokenRequestDto;
 import com.example.jwtsecurity.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
