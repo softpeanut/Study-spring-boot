3b58610ccfc5684e4c12ad1801c34b6c12cb4505
김범진 <sdpthf@naver.com>
2021-08-12 20:57:13 +0900

    JwtPracticeApplicationTests

---
+++ jwt-practice/src/test/java/com/example/jwtpractice2/JwtPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtpractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
