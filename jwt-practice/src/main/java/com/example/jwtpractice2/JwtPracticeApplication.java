375cf8102d2b1319273f8c98a78278b6ed5c5869
김범진 <sdpthf@naver.com>
2021-08-12 20:56:15 +0900

    JwtPracticeApplication

---
+++ jwt-practice/src/main/java/com/example/jwtpractice2/JwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtPractice2Application.class, args);
+    }
+
+}
