76ab7b5e093246aa74761a93dfa6f975ccf6e20f
김범진 <sdpthf@naver.com>
2021-08-12 23:02:45 +0900

    JwtTokenProvider

--- jwt-practice/src/main/java/com/example/jwtpractice2/jwt/JwtTokenProvider.java
+++ jwt-practice/src/main/java/com/example/jwtpractice2/jwt/JwtTokenProvider.java
@@ -1,4 +1,70 @@
 package com.example.jwtpractice2.jwt;
 
+import com.example.jwtpractice2.dto.TokenResponse;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
+    private final UserDetailsService userDetailsService;
+    private Long tokenExpiration = 1000L * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secretkey;
+
+    protected String init() {
+        return Base64.getEncoder().encodeToString(secretkey.getBytes());
+    }
+
+    public TokenResponse createToken(String userPk, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(userPk);
+        claims.put("roles", roles);
+        Date now = new Date();
+
+        String token = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, init())
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenExpiration))
+                .compact();
+
+        return new TokenResponse(token);
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(secretkey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretkey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
