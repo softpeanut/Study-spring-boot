f25f0498a5e6d1c2b5fd197e71bfe413564535c1
김범진 <sdpthf@naver.com>
2021-08-12 22:28:24 +0900

    TokenResponse

--- jwt-practice/src/main/java/com/example/jwtpractice2/dto/TokenResponse.java
+++ jwt-practice/src/main/java/com/example/jwtpractice2/dto/TokenResponse.java
@@ -1,4 +1,12 @@
 package com.example.jwtpractice2.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenResponse {
+    private String token;
 }
