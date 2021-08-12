ea1fbe353d94d2d76dda259adaafb29ead412152
김범진 <sdpthf@naver.com>
2021-08-12 22:29:07 +0900

    CustomUserDetailsService

--- jwt-practice/src/main/java/com/example/jwtpractice2/service/CustomUserDetailsService.java
+++ jwt-practice/src/main/java/com/example/jwtpractice2/service/CustomUserDetailsService.java
@@ -1,4 +1,20 @@
 package com.example.jwtpractice2.service;
 
-public class CustomUserDetailsService   {
+import com.example.jwtpractice2.entity.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
 }
