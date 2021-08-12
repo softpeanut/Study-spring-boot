8f6a18f9d7b53a17528955bf82e2127ecbcdb588
김범진 <sdpthf@naver.com>
2021-08-12 22:28:09 +0900

    AuthController

--- jwt-practice/src/main/java/com/example/jwtpractice2/controller/AuthController.java
+++ jwt-practice/src/main/java/com/example/jwtpractice2/controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.example.jwtpractice2.controller;
 
+import com.example.jwtpractice2.dto.MemberRequest;
+import com.example.jwtpractice2.dto.TokenResponse;
+import com.example.jwtpractice2.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/join")
+    public String join(@RequestBody MemberRequest request) {
+        return authService.join(request);
+    }
+
+    @PostMapping("/login")
+    public TokenResponse login(@RequestBody MemberRequest request) {
+        return authService.login(request);
+    }
+
 }
