88cb48ad69e14ac47bb0efbae50a77826e528a75
김범진 <sdpthf@naver.com>
2021-08-12 22:28:16 +0900

    MemberRequest

--- jwt-practice/src/main/java/com/example/jwtpractice2/dto/MemberRequest.java
+++ jwt-practice/src/main/java/com/example/jwtpractice2/dto/MemberRequest.java
@@ -1,4 +1,13 @@
 package com.example.jwtpractice2.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberRequest {
+    private String email;
+    private String password;
 }
