77d92e5505a5c5e95b5e1109b513d9d7f75605d8
김범진 <sdpthf@naver.com>
2021-08-13 15:30:44 +0900

    UserController

--- jwt-practice/src/main/java/com/example/jwtpractice2/controller/UserController.java
+++ jwt-practice/src/main/java/com/example/jwtpractice2/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.example.jwtpractice2.controller;
 
+import com.example.jwtpractice2.dto.UserResponse;
+import com.example.jwtpractice2.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> me() {
+        return ResponseEntity.ok(userService.me());
+    }
+
 }
