77e8490ee9bed10865546bcc7c5827a2aef2902a
김범진 <sdpthf@naver.com>
2021-08-13 22:56:21 +0900

    Config

--- jwt-practice/src/main/java/com/example/jwtpractice2/config/SecurityConfig.java
+++ jwt-practice/src/main/java/com/example/jwtpractice2/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .httpBasic().disable()
                 .csrf().disable()
+                .formLogin().disable()
 
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
