d3ebe2fe8787fec0bffccd6920d006b1a1233f54
김범진 <sdpthf@naver.com>
2021-08-14 14:56:37 +0900

    JwtAuthenticationFilter

--- jwt-practice/src/main/java/com/example/jwtpractice2/jwt/JwtAuthenticationFilter.java
+++ jwt-practice/src/main/java/com/example/jwtpractice2/jwt/JwtAuthenticationFilter.java
@@ -4,27 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
+    public void doFilterInternal(HttpServletRequest request,
+                         HttpServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
 
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
-
+        String token = jwtTokenProvider.resolveToken(request);
+        System.out.println("안녕");
         if(token!=null && jwtTokenProvider.validateToken(token)) {
+            System.out.println("하이");
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            System.out.println("헬로우");
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
