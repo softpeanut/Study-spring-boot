3328a5f54499285724f91f6439341812f89deb7f
김범진 <sdpthf@naver.com>
2021-08-15 12:08:35 +0900

    [ADD] ExceptionResponse

---
+++ jwt-practice/src/main/java/com/example/jwtpractice2/exception/response/ExceptionResponse.java
@@ -0,0 +1,10 @@
+package com.example.jwtpractice2.exception.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExceptionResponse {
+    private String message;
+}
