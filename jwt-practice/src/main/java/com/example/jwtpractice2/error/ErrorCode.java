9b1a4f54477b030b58ff1f7207ebf24e72757f3c
김범진 <sdpthf@naver.com>
2021-08-15 12:07:00 +0900

    [ADD] ErrorCode

---
+++ jwt-practice/src/main/java/com/example/jwtpractice2/error/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.example.jwtpractice2.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INVALID_TOKEN(401, "Invalid Token"),
+    INVALID_PASSWORD(401, "Invalid Password"),
+
+    USER_ALREADY_EXISTS(409, "User Already Exists"),
+    USER_NOT_FOUND(404, "User Not Found");
+
+    private final Integer status;
+    private final String message;
+}
