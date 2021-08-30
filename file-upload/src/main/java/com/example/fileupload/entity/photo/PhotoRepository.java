471fd606e3b43f1fa2937f2a6496e49e870227de
김범진 <sdpthf@naver.com>
2021-08-30 10:31:13 +0900

    [ADD] PhotoRepository

---
+++ file-upload/src/main/java/com/example/fileupload/entity/photo/PhotoRepository.java
@@ -0,0 +1,6 @@
+package com.example.fileupload.entity.photo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+}
