a912cab2e64b2eee61c2a9a5b458f0ad6997710e
김범진 <sdpthf@naver.com>
2021-10-10 21:16:17 +0900

    [UPDATE] MailService

--- mail-sender/src/main/java/com/example/mailsender/service/MailService.java
+++ mail-sender/src/main/java/com/example/mailsender/service/MailService.java
@@ -1,7 +1,25 @@
 package com.example.mailsender.service;
 
+import com.example.mailsender.payload.request.SignupRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MailService {
+    private final JavaMailSender javaMailSender;
+    private static Integer certificationNumber = 123456;
+
+    @Async
+    public void sendEmail(SignupRequest request) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(request.getEmail());
+        message.setSubject("[Test 이메일 인증]");
+        message.setText("귀하의 이메일 인증번호는 " + certificationNumber + "입니다.");
+        javaMailSender.send(message);
+    }
+
 }
