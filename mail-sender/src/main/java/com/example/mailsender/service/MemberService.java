bc794bb07021ab129838e4d3f5bb01b8fb832186
김범진 <sdpthf@naver.com>
2021-10-10 23:08:03 +0900

    [UPDATE] MemberService

--- mail-sender/src/main/java/com/example/mailsender/service/MemberService.java
+++ mail-sender/src/main/java/com/example/mailsender/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.mailsender.service;
 import com.example.mailsender.payload.request.SignupRequest;
 
 public interface MemberService {
-    void verifyAccount(String token);
+    void sendEmail(String email);
+    boolean verifyAccount(String code);
     void signup(SignupRequest request);
 }
--- mail-sender/src/main/java/com/example/mailsender/service/MemberServiceImpl.java
+++ mail-sender/src/main/java/com/example/mailsender/service/MemberServiceImpl.java
@@ -1,11 +1,12 @@
 package com.example.mailsender.service;
 
-import com.example.mailsender.entity.Member;
-import com.example.mailsender.entity.MemberRepository;
+import com.example.mailsender.entity.member.Member;
+import com.example.mailsender.entity.member.MemberRepository;
 import com.example.mailsender.payload.request.SignupRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,14 +14,26 @@ public class MemberServiceImpl implements MemberService {
 
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    private final MailService mailService;
 
     @Override
-    public void verifyAccount(String token) {
-
+    public void sendEmail(String email) {
+        mailService.sendEmail(email);
     }
 
     @Override
+    public boolean verifyAccount(String code) {
+        return code.equals(MailService.createKey());
+    }
+
+    @Override
+    @Transactional
     public void signup(SignupRequest request) {
+        if(memberRepository.findByEmail(request.getEmail()).isPresent())
+            throw new IllegalArgumentException("이메일이 이미 존재합니다.");
+        else if(memberRepository.findByName(request.getName()).isPresent())
+            throw new IllegalArgumentException("닉네임이 이미 존재합니다.");
+
         memberRepository.save(Member.builder()
                 .name(request.getName())
                 .email(request.getEmail())
