d25e5c0e456e14d7386c3904013369fd34922f6e
김범진 <sdpthf@naver.com>
2021-10-10 23:03:12 +0900

    [ADD] FindByName

--- mail-sender/src/main/java/com/example/mailsender/entity/member/MemberRepository.java
+++ mail-sender/src/main/java/com/example/mailsender/entity/member/MemberRepository.java
@@ -5,4 +5,5 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     Optional<Member> findByEmail(String email);
+    Optional<Member> findByName(String name);
 }
