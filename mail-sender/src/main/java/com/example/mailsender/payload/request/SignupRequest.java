16012ef513005f836e4981424bab565b3983d472
김범진 <sdpthf@naver.com>
2021-10-10 23:07:01 +0900

    [UPDATE] Signup Request Not Blank

--- mail-sender/src/main/java/com/example/mailsender/payload/request/SignupRequest.java
+++ mail-sender/src/main/java/com/example/mailsender/payload/request/SignupRequest.java
@@ -2,11 +2,18 @@ package com.example.mailsender.payload.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
 public class SignupRequest {
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 }
