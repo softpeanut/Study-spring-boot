8ce0df81218e92a2d832ab788f2b22501af6fee9
김범진 <sdpthf@naver.com>
2021-10-10 18:17:51 +0900

    [DELETE] Security

--- mail-sender/src/main/java/com/example/mailsender/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.mailsender.entity;
-
-public enum Role {
-    ROLE_USER, ROLE_ADMIN
-}
--- mail-sender/src/main/java/com/example/mailsender/security/jwt/JwtTokenFilter.java
+++
@@ -1,4 +0,0 @@
-package com.example.mailsender.security.jwt;
-
-public class JwtTokenFilter {
-}
