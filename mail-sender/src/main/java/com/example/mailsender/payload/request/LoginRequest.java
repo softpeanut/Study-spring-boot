663d1710305f5cbfb1577abe27cc230cb81e7906
김범진 <sdpthf@naver.com>
2021-10-10 18:11:27 +0900

    [ADD] LoginRequest

---
+++ mail-sender/src/main/java/com/example/mailsender/payload/request/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.example.mailsender.payload.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
