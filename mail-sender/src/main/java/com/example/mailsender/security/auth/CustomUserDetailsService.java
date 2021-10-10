ae5c7305f8114f0e0ebb8d88398457559c4dd257
김범진 <sdpthf@naver.com>
2021-10-10 18:11:51 +0900

    [ADD] CustomUserDetailsService

---
+++ mail-sender/src/main/java/com/example/mailsender/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.mailsender.security.auth;
+
+import com.example.mailsender.entity.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email)
+                .orElseThrow();
+    }
+
+}
