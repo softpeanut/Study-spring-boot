1c927974532ddc115f115f3f3f0351a484c30b01
김범진 <sdpthf@naver.com>
2021-10-10 23:51:47 +0900

    [UPDATE] MemberService

--- mail-sender/src/main/java/com/example/mailsender/service/MemberService.java
+++ mail-sender/src/main/java/com/example/mailsender/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.example.mailsender.service;
 
+import com.example.mailsender.payload.request.EmailRequest;
+import com.example.mailsender.payload.request.EmailVerifiedRequest;
 import com.example.mailsender.payload.request.SignupRequest;
 
 public interface MemberService {
-    void sendEmail(String email);
-    boolean verifyAccount(String code);
+    void sendEmail(EmailRequest request);
+    void verifyAccount(EmailVerifiedRequest request);
     void signup(SignupRequest request);
 }
--- mail-sender/src/main/java/com/example/mailsender/service/MemberServiceImpl.java
+++ mail-sender/src/main/java/com/example/mailsender/service/MemberServiceImpl.java
@@ -1,7 +1,15 @@
 package com.example.mailsender.service;
 
+import com.example.mailsender.entity.certification.Certification;
+import com.example.mailsender.entity.certification.CertificationRepository;
+import com.example.mailsender.entity.certification.Certified;
 import com.example.mailsender.entity.member.Member;
 import com.example.mailsender.entity.member.MemberRepository;
+import com.example.mailsender.exception.EmailNotFoundException;
+import com.example.mailsender.exception.MemberEmailAlreadyExistsException;
+import com.example.mailsender.exception.MemberNameAlreadyExistsException;
+import com.example.mailsender.payload.request.EmailRequest;
+import com.example.mailsender.payload.request.EmailVerifiedRequest;
 import com.example.mailsender.payload.request.SignupRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,31 +22,45 @@ public class MemberServiceImpl implements MemberService {
 
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    private final CertificationRepository certificationRepository;
     private final MailService mailService;
 
     @Override
-    public void sendEmail(String email) {
-        mailService.sendEmail(email);
+    public void sendEmail(EmailRequest request) {
+        mailService.sendEmail(request.getEmail());
     }
 
     @Override
-    public boolean verifyAccount(String code) {
-        return code.equals(MailService.createKey());
+    @Transactional
+    public void verifyAccount(EmailVerifiedRequest request) {
+        Certification certification = certificationRepository.findByEmail(request.getEmail())
+                .orElseThrow(EmailNotFoundException::new);
+
+        if(request.getCode() == certification.getCode()) {
+               certification.updateCertified(Certified.CERTIFIED);
+        }
     }
 
     @Override
     @Transactional
     public void signup(SignupRequest request) {
         if(memberRepository.findByEmail(request.getEmail()).isPresent())
-            throw new IllegalArgumentException("이메일이 이미 존재합니다.");
+            throw new MemberEmailAlreadyExistsException();
         else if(memberRepository.findByName(request.getName()).isPresent())
-            throw new IllegalArgumentException("닉네임이 이미 존재합니다.");
+            throw new MemberNameAlreadyExistsException();
 
-        memberRepository.save(Member.builder()
-                .name(request.getName())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .build());
+        Certified certified = certificationRepository.findByEmail(request.getEmail())
+                .map(certification -> certification.getCertified())
+                .orElseThrow(EmailNotFoundException::new);
+
+        if(certified == Certified.CERTIFIED) {
+            memberRepository.save(Member.builder()
+                    .name(request.getName())
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .build());
+        }
+
     }
 
 }
