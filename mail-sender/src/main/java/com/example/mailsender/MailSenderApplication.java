91f21501b42eee95d9b2bff6933cc73918b2c960
김범진 <sdpthf@naver.com>
2021-10-10 17:01:18 +0900

    Settings

---
+++ mail-sender/src/main/java/com/example/mailsender/MailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSenderApplication.class, args);
+    }
+
+}
