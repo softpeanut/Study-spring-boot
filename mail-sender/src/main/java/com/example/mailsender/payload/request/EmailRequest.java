b0bcc394eaa10079b665eb57150cde7e98ac4892
김범진 <sdpthf@naver.com>
2021-10-10 23:51:06 +0900

    [ADD] EmailRequest

---
+++ mail-sender/src/main/java/com/example/mailsender/payload/request/EmailRequest.java
@@ -0,0 +1,10 @@
+package com.example.mailsender.payload.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailRequest {
+    private String email;
+}
