95db9622b926125a1f143e0ed0fdeace62e4bd46
김범진 <sdpthf@naver.com>
2021-10-10 18:12:43 +0900

    [ADD] MemberRepository

---
+++ mail-sender/src/main/java/com/example/mailsender/entity/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.mailsender.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByEmail(String email);
+}
