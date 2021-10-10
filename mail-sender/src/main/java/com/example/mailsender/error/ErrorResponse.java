20543f8d89cd6f94601d54e36bd3dae5bab75b54
김범진 <sdpthf@naver.com>
2021-10-10 18:12:25 +0900

    [ADD] ErrorResponse

---
+++ mail-sender/src/main/java/com/example/mailsender/error/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.example.mailsender.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponse {
+    private final String message;
+}
