2a531c1a400723b2ebea15c2ccbdc703f7887fe6
김범진 <sdpthf@naver.com>
2021-10-10 18:11:40 +0900

    [ADD] TokenResponse

---
+++ mail-sender/src/main/java/com/example/mailsender/payload/response/TokenResponse.java
@@ -0,0 +1,10 @@
+package com.example.mailsender.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private String token;
+}
