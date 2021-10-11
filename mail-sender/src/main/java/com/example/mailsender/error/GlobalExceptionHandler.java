81deb248f4d57897517680a106d5de081962609f
김범진 <sdpthf@naver.com>
2021-10-11 21:14:50 +0900

    [ADD] Valid Exception Handling

--- mail-sender/src/main/java/com/example/mailsender/error/GlobalExceptionHandler.java
+++ mail-sender/src/main/java/com/example/mailsender/error/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.mailsender.error;
 import com.example.mailsender.error.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +14,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorrResponse> handleException(BusinessException e) {
         final ErrorCode errorCode = e.getErrorCode();
         return new ResponseEntity<>(new ErrorrResponse(errorCode.getMessage()), HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorrResponse> handleValidException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(
+                new ErrorrResponse(e.getBindingResult().getAllErrors().get(0).getDefaultMessage()), HttpStatus.BAD_REQUEST);
     }
 
 }
