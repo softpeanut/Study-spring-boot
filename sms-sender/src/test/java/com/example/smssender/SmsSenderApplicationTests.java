cdb61880af1885db2f5e8da50b8f444bb7ad0ab3
김범진 <sdpthf@naver.com>
2021-10-14 09:17:05 +0900

    Init Project

---
+++ sms-sender/src/test/java/com/example/smssender/SmsSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smssender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
