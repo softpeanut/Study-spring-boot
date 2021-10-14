e6b514d0f4904309511b5045590e43e89cbb7b32
김범진 <sdpthf@naver.com>
2021-10-15 08:35:30 +0900

    [ADD] GlobalExceptionHandler

---
+++ sms-sender/src/main/java/com/example/smssender/error/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.smssender.error;
+
+import com.example.smssender.error.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleException(BusinessException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        return new ResponseEntity<>(new ErrorResponse(errorCode.getMessage()), HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> validException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getBindingResult().getAllErrors().get(0).getDefaultMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+}
