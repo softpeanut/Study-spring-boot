f6bb2735f342f06b1a016c2057e3482c168a1ee7
김범진 <sdpthf@naver.com>
2021-10-14 09:34:26 +0900

    [ADD] Certification

---
+++ sms-sender/src/main/java/com/example/smssender/entity/certification/Certification.java
@@ -0,0 +1,38 @@
+package com.example.smssender.entity.certification;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash
+public class Certification implements Serializable {
+
+    @Id @Indexed
+    private String phoneNumber;
+    private String code;
+    private Certified certified;
+
+    @TimeToLive
+    private Integer codeExp;
+
+    public Certification updateCertified(Certified certified) {
+        this.certified=certified;
+        return this;
+    }
+
+    public Certification updateCode(String code) {
+        this.code = code;
+        return this;
+    }
+
+}
