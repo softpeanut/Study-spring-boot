2b97c04bebb0f9dc4d85c0fd4dc857bacd443290
김범진 <sdpthf@naver.com>
2021-10-15 08:35:44 +0900

    [ADD] ErrorResponse

---
+++ sms-sender/src/main/java/com/example/smssender/error/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.example.smssender.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
