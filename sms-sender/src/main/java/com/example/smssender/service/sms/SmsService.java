688cf377246ce02fb44610c4c3e038bd40ef3b6b
김범진 <sdpthf@naver.com>
2021-10-14 10:09:56 +0900

    [ADD] SmsService

---
+++ sms-sender/src/main/java/com/example/smssender/service/sms/SmsService.java
@@ -0,0 +1,43 @@
+package com.example.smssender.service.sms;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+
+@Service
+public class SmsService {
+
+    @Value("${coolsms.api.key}")
+    private String apiKey;
+
+    @Value("${coolsms.api.secret}")
+    private String apiSecret;
+
+    @Value("${coolsms.phone-number}")
+    private String fromNumber;
+
+    public void sendMessage(String toNumber, String randomNumber) {
+
+        Message coolsms = new Message(apiKey, apiSecret);
+
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("to", toNumber);
+        params.put("from", fromNumber);
+        params.put("type", "SMS");
+        params.put("text", "[grabMe] 인증번호 "+randomNumber+" 를 입력하세요.");
+        params.put("app_version", "test app 1.2"); // application name and version
+
+        try {
+            JSONObject obj = (JSONObject) coolsms.send(params);
+            System.out.println(obj.toString());
+        } catch (CoolsmsException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+        }
+
+    }
+
+}
