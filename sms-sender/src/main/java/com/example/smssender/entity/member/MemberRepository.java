f20bf02b26e115b9d617c21ddb02bdceaae44c92
김범진 <sdpthf@naver.com>
2021-10-14 19:34:51 +0900

    [ADD] findByEmail && findByPhoneNumber

--- sms-sender/src/main/java/com/example/smssender/entity/member/MemberRepository.java
+++ sms-sender/src/main/java/com/example/smssender/entity/member/MemberRepository.java
@@ -1,6 +1,10 @@
 package com.example.smssender.entity.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByPhoneNumber(String phoneNumber);
+    Optional<Member> findByName(String name);
+
 }
