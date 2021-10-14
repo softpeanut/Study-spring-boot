0903a1cca0ebd31ab2f5dd08ac4f6857a4f10efa
김범진 <sdpthf@naver.com>
2021-10-14 16:52:50 +0900

    [ADD] Send Code && Resend Code Controller

--- sms-sender/src/main/java/com/example/smssender/controller/MemberController.java
+++ sms-sender/src/main/java/com/example/smssender/controller/MemberController.java
@@ -2,13 +2,24 @@ package com.example.smssender.controller;
 
 import com.example.smssender.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
 
+    @GetMapping("/code")
+    public void sendCode(String phoneNumber) {
+        memberService.sendCode(phoneNumber);
+    }
 
+    @GetMapping("/code/re")
+    public void resendCode(String phoneNumber) {
+        memberService.resendCode(phoneNumber);
+    }
 
 }
