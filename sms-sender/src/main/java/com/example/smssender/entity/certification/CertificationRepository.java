2b2376b306171eee88ed8f33e384388daeabecf4
김범진 <sdpthf@naver.com>
2021-10-14 09:34:49 +0900

    [ADD] CertificationRepository

---
+++ sms-sender/src/main/java/com/example/smssender/entity/certification/CertificationRepository.java
@@ -0,0 +1,10 @@
+package com.example.smssender.entity.certification;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CertificationRepository extends CrudRepository<Certification, String> {
+    Optional<Certification> findByPhoneNumber(String phoneNumber);
+}
