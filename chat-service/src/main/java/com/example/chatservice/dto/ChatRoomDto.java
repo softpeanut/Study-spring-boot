809492763d52e0d21dd086b6a9c2056848b45a4b
김범진 <sdpthf@naver.com>
2021-10-18 09:20:20 +0900

    [ADD] ChatRoomDto

---
+++ chat-service/src/main/java/com/example/chatservice/dto/ChatRoomDto.java
@@ -0,0 +1,23 @@
+package com.example.chatservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.socket.WebSocketSession;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter @Setter
+public class ChatRoomDto {
+    private String roomId;
+    private String name;
+    private Set<WebSocketSession> sessions = new HashSet<>();
+
+    public static ChatRoomDto create(String name) {
+        ChatRoomDto room = new ChatRoomDto();
+
+        room.roomId = UUID.randomUUID().toString();
+        room.name = name;
+        return room;
+    }
+}
