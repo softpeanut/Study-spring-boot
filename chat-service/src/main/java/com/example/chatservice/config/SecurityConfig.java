00c0e5db8bcf124147fe3ad7974b10f487c367e3
김범진 <sdpthf@naver.com>
2021-10-18 09:18:55 +0900

    [ADD] SecurityConfig

---
+++ chat-service/src/main/java/com/example/chatservice/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.chatservice.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Log4j2
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+
+}
