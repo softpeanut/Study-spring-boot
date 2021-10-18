5fd1ad9774672c5bfd977dd6a536508debf660d0
김범진 <sdpthf@naver.com>
2021-10-18 09:20:12 +0900

    [ADD] ChatMessageDto

---
+++ chat-service/src/main/java/com/example/chatservice/dto/ChatMessageDto.java
@@ -0,0 +1,11 @@
+package com.example.chatservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ChatMessageDto {
+    private String roomId;
+    private String writer;
+    private String message;
+}
