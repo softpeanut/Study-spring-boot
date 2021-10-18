24c7c9311bbb678fda3dbe56b2f89273ffa3817f
김범진 <sdpthf@naver.com>
2021-10-18 09:20:04 +0900

    [ADD] ChatRoomController

---
+++ chat-service/src/main/java/com/example/chatservice/controller/ChatRoomController.java
@@ -0,0 +1,47 @@
+package com.example.chatservice.controller;
+
+import com.example.chatservice.repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Log4j2
+@RestController
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChatRoomController {
+
+    private final ChatRoomRepository repository;
+
+    @GetMapping(value = "/rooms")
+    public ModelAndView rooms(){
+
+        log.info("# All Chat Rooms");
+        ModelAndView mv = new ModelAndView("chat/rooms");
+
+        mv.addObject("list", repository.findAllRooms());
+
+        return mv;
+    }
+
+    @PostMapping(value = "/room")
+    public String create(@RequestParam String name, RedirectAttributes rttr){
+
+        log.info("# Create Chat Room , name: " + name);
+        rttr.addFlashAttribute("roomName", repository.createChatRoomDto(name));
+        return "redirect:/chat/rooms";
+    }
+
+    @GetMapping("/room")
+    public void getRoom(String roomId, Model model){
+
+        log.info("# get Chat Room, roomID : " + roomId);
+
+        model.addAttribute("room", repository.findRoomById(roomId));
+    }
+
+}
+
