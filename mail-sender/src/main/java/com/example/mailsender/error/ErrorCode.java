3e36f19f7d05893a9fbf6eb04649791ac26e3118
김범진 <sdpthf@naver.com>
2021-10-18 19:34:37 +0900

    [DELETE] EmailNotFoundException

--- mail-sender/src/main/java/com/example/mailsender/error/ErrorCode.java
+++ mail-sender/src/main/java/com/example/mailsender/error/ErrorCode.java
@@ -10,7 +10,6 @@ public enum ErrorCode {
     MEMBER_NAME_ALREADY_EXISTS(409, "Member Name Already Exists"),
     MEMBER_EMAIL_ALREADY_EXISTS(409, "Member Email Already Exists"),
 
-    EMAIL_NOT_FOUND(404, "Email Not Found"),
     EMAIL_NOT_CERTIFIED(401, "Email Not Certified"),
     SEND_MESSAGE_FAILED(500, "Send Message Failed"),
 
