5eee37738c35e1ae408e2584db66f0c04f19254b
김범진 <sdpthf@naver.com>
2021-10-21 09:48:57 +0900

    [ADD] StompChatController

---
+++ chat-service/src/main/java/com/example/chatservice/controller/StompChatController.java
@@ -0,0 +1,26 @@
+package com.example.chatservice.controller;
+
+import com.example.chatservice.dto.ChatMessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StompChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat/enter")
+    public void enter(ChatMessageDto message) {
+        message.setMessage(message.getWriter() + "님이 채팅방에 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room");
+    }
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessageDto message) {
+        messagingTemplate.convertAndSend("/sub/chat/room" + message.getRoomId(), message);
+    }
+
+}
