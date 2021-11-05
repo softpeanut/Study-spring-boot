1acf32b875a9596f16cc9b041128f4dba501ad4a
김범진 <sdpthf@naver.com>
2021-11-06 00:10:17 +0900

    ⚡️ :: save && findAll

--- jpa-optimization/src/main/java/com/inflearn/jpaoptimization/repository/OrderRepository.java
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/repository/OrderRepository.java
@@ -1,7 +1,25 @@
 package com.inflearn.jpaoptimization.repository;
 
+import com.inflearn.jpaoptimization.domain.order.Order;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Repository
 public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class)
+                .getResultList();
+    }
+
 }
