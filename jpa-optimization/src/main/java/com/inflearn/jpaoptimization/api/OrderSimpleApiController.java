bb84f05ad408c0fb277c51005a38c6a5b1cb7c20
김범진 <sdpthf@naver.com>
2021-11-06 00:10:28 +0900

    📑 ::OrderSimpleApiController

---
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderSimpleApiController.java
@@ -0,0 +1,27 @@
+package com.inflearn.jpaoptimization.api;
+
+import com.inflearn.jpaoptimization.domain.order.Order;
+import com.inflearn.jpaoptimization.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAll();
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
+        return all;
+    }
+
+}
