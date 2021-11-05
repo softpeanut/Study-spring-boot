f535353be341883b81b2ec59ebc82eb29a4160a1
김범진 <sdpthf@naver.com>
2021-11-05 23:58:18 +0900

    📑 :: OrderItem

---
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/domain/OrderItem.java
@@ -0,0 +1,34 @@
+package com.inflearn.jpaoptimization.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @JsonIgnore
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+
+}
