6d76a47c3a7a8cfe5736abb91bc0c42d4e40340a
김범진 <sdpthf@naver.com>
2021-11-06 14:23:40 +0900

    ⚡️ :: 설명 추가

--- jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderSimpleApiController.java
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderSimpleApiController.java
@@ -10,12 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * xToOne(ManyToOne, OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
 @RequiredArgsConstructor
 @RestController
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAll();
@@ -26,6 +37,10 @@ public class OrderSimpleApiController {
         return all;
     }
 
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2() {
         return orderRepository.findAll().stream()
