fe9306d1010ab88fd0bc70d54d7165fc5750be16
김범진 <sdpthf@naver.com>
2021-11-06 15:07:04 +0900

    ⚡️ :: OrdersV2

--- jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderApiController.java
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderApiController.java
@@ -1,13 +1,14 @@
 package com.inflearn.jpaoptimization.api;
 
+import com.inflearn.jpaoptimization.api.dto.OrderDto;
 import com.inflearn.jpaoptimization.domain.order.Order;
-import com.inflearn.jpaoptimization.domain.order.OrderItem;
 import com.inflearn.jpaoptimization.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * V1. 엔티티 직접 노출
@@ -50,6 +51,17 @@ public class OrderApiController {
                     .forEach(o -> o.getItem().getName());
         }
         return all;
+    }
+
+    /**
+     * V2.엔티티를 조회해서 DTO로 변환 (fetch join 사용 X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출 (N+1문제 발생)
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        return orderRepository.findAll().stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
     }
 
 }
