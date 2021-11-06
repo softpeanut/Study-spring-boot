642f9f3f53d929bd3d98e5300a5c477aa204ea11
김범진 <sdpthf@naver.com>
2021-11-06 14:27:12 +0900

    ⚡️ :: OrdersV3

--- jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderSimpleApiController.java
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderSimpleApiController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 /**
  * xToOne(ManyToOne, OneToOne) 관계 최적화
  * Order
@@ -23,8 +25,7 @@ public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
 
     /**
-     * V1. 엔티티 직접 노출
-     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * V1.엔티티 직접 노출
      * - 양방향 관계 문제 발생 -> @JsonIgnore
      */
     @GetMapping("/api/v1/simple-orders")
@@ -38,14 +39,27 @@ public class OrderSimpleApiController {
     }
 
     /**
-     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
-     * - 단점: 지연로딩으로 쿼리 N번 호출
+     * V2.엔티티를 조회해서 DTO로 변환 (fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출 (N+1문제 발생)
      */
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2() {
         return orderRepository.findAll().stream()
                 .map(o -> new SimpleOrderDto(o))
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * V3.엔티티를 조회해서 DTO로 변환 (fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
     }
 
 }
