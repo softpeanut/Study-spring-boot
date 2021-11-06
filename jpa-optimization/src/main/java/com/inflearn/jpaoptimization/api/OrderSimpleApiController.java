50760a7635fc644dfb496d355bc23fe8844c3c65
김범진 <sdpthf@naver.com>
2021-11-06 14:37:04 +0900

    ⚡️ :: OrdersV4

--- jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderSimpleApiController.java
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderSimpleApiController.java
@@ -1,8 +1,10 @@
 package com.inflearn.jpaoptimization.api;
 
+import com.inflearn.jpaoptimization.api.dto.OrderSimpleQueryDto;
 import com.inflearn.jpaoptimization.api.dto.SimpleOrderDto;
 import com.inflearn.jpaoptimization.domain.order.Order;
 import com.inflearn.jpaoptimization.repository.OrderRepository;
+import com.inflearn.jpaoptimization.repository.OrderSimpleQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,7 @@ import static java.util.stream.Collectors.toList;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     /**
      * V1.엔티티 직접 노출
@@ -39,7 +42,7 @@ public class OrderSimpleApiController {
     }
 
     /**
-     * V2.엔티티를 조회해서 DTO로 변환 (fetch join 사용X)
+     * V2.엔티티를 조회해서 DTO로 변환 (fetch join 사용 X)
      * - 단점: 지연로딩으로 쿼리 N번 호출 (N+1문제 발생)
      */
     @GetMapping("/api/v2/simple-orders")
@@ -50,7 +53,7 @@ public class OrderSimpleApiController {
     }
 
     /**
-     * V3.엔티티를 조회해서 DTO로 변환 (fetch join 사용O)
+     * V3.엔티티를 조회해서 DTO로 변환 (fetch join 사용 O)
      * - fetch join으로 쿼리 1번 호출
      */
     @GetMapping("/api/v3/simple-orders")
@@ -60,6 +63,15 @@ public class OrderSimpleApiController {
                 .map(o -> new SimpleOrderDto(o))
                 .collect(toList());
         return result;
+    }
+
+    /**
+     * V4.DTO로 조회 (fetch join 사용 X)
+     *
+     */
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
 }
