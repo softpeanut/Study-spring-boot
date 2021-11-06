99abe09294487ec218f4bc9f074cf267b4ac0c83
김범진 <sdpthf@naver.com>
2021-11-06 15:06:50 +0900

    📑 :: OrderDto

---
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/dto/OrderDto.java
@@ -0,0 +1,49 @@
+package com.inflearn.jpaoptimization.api.dto;
+
+import com.inflearn.jpaoptimization.domain.delivery.Address;
+import com.inflearn.jpaoptimization.domain.order.Order;
+import com.inflearn.jpaoptimization.domain.order.OrderItem;
+import com.inflearn.jpaoptimization.domain.order.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Data
+public class OrderDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemDto> orderItems;
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
+    public OrderDto(Order order) {
+        orderId = order.getId();
+        name = order.getMember().getName();
+        orderDate = order.getOrderDate();
+        orderStatus = order.getStatus();
+        address = order.getDelivery().getAddress();
+        orderItems = order.getOrderItems().stream()
+                .map(orderItem -> new OrderItemDto(orderItem))
+                .collect(toList());
+    }
+
+}
