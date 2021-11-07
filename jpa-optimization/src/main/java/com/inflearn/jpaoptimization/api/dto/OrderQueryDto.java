e6adfac24a06cd94e080348a9948bc5c484c5225
김범진 <sdpthf@naver.com>
2021-11-07 22:37:42 +0900

    📑 :: OrderQueryDto

---
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/dto/OrderQueryDto.java
@@ -0,0 +1,30 @@
+package com.inflearn.jpaoptimization.api.dto;
+
+import com.inflearn.jpaoptimization.domain.delivery.Address;
+import com.inflearn.jpaoptimization.domain.order.OrderStatus;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(of = "orderId")
+public class OrderQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+
+}
