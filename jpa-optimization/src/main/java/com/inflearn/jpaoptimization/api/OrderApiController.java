75d04fc0932c21727061300bd4ac8ca6ad3942b5
김범진 <sdpthf@naver.com>
2021-11-07 23:19:56 +0900

    ⚡️ :: OrdersV5

--- jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderApiController.java
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/OrderApiController.java
@@ -100,4 +100,9 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
 }
