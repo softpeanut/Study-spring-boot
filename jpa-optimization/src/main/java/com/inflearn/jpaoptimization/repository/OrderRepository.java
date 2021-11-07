d7a5f955fc1f65f1f13c1f29c75e0392f6791d3c
김범진 <sdpthf@naver.com>
2021-11-07 22:43:15 +0900

    ⚡️ :: findAllWithMemberDelivery

--- jpa-optimization/src/main/java/com/inflearn/jpaoptimization/repository/OrderRepository.java
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/repository/OrderRepository.java
@@ -30,6 +30,16 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
     public List<Order> findAllWithItem() {
         return em.createQuery(
                 "select distinct o from Order o" +
