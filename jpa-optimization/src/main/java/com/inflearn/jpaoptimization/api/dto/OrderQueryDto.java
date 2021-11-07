832dc08e341be6e73cba064beaba82cfd4e7d6fb
김범진 <sdpthf@naver.com>
2021-11-07 23:26:44 +0900

    ⚡️ :: 생성자

--- jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/dto/OrderQueryDto.java
+++ jpa-optimization/src/main/java/com/inflearn/jpaoptimization/api/dto/OrderQueryDto.java
@@ -27,4 +27,13 @@ public class OrderQueryDto {
         this.address = address;
     }
 
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
+
 }
