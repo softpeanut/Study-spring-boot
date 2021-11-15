fc7c67a90465827938ce70ce4da2adf8436473b8
김범진 <sdpthf@naver.com>
2021-11-15 19:26:55 +0900

    ⚙️ :: Init Project

---
+++ fcm-server/src/test/java/com/example/fcmserver/FcmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
