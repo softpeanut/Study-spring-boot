412e267b15299e6cae6200fa17a0b0e1ad20a4c5
김범진 <sdpthf@naver.com>
2021-11-16 08:40:14 +0900

    📑 :: JwtTokenProvider

---
+++ fcm-server/src/main/java/com/example/fcmserver/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,84 @@
+package com.example.fcmserver.security.jwt;
+
+import com.example.fcmserver.security.auth.CustomUserDetailsService;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private String secretKey;
+
+    @Value("${jwt.access.exp}")
+    private Long accessExp;
+
+    @Value("${jwt.header}")
+    private String header;
+
+    @Value("${jwt.prefix}")
+    private String prefix;
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Value("${jwt.secret}")
+    private void setSecretKey(String secretKey) {
+        this.secretKey =  Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(String email) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setHeaderParam("typ", "JWT")
+                .setSubject(email)
+                .claim("type", "access")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessExp * 1000))
+                .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader(header);
+        if(bearer != null && bearer.length() > 7 && bearer.startsWith(prefix)) {
+            return bearer.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        return getTokenBody(token)
+                .getExpiration().after(new Date());
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = customUserDetailsService
+                .loadUserByUsername(getTokenSubject(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private Claims getTokenBody(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            throw new IllegalArgumentException("Expired Token");
+        } catch (MalformedJwtException | SignatureException e) {
+            throw new IllegalArgumentException("Invalid Token");
+        }
+
+    }
+
+    private String getTokenSubject(String token) {
+        return getTokenBody(token).getSubject();
+    }
+
+}
