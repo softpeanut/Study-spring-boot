cd1b12cbaf315ed9e846605b1244ac6242e91e60
김범진 <sdpthf@naver.com>
2021-11-16 08:40:29 +0900

    📑 :: CustomUserDetailsService

---
+++ fcm-server/src/main/java/com/example/fcmserver/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.fcmserver.security.auth;
+
+import com.example.fcmserver.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findById(username)
+                .orElseThrow(() -> new IllegalArgumentException("user not found"));
+    }
+
+}
