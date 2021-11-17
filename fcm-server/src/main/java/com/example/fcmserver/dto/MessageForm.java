c27d75ab8fa8f2da8ba53d8fda8a9393504ac291
김범진 <sdpthf@naver.com>
2021-11-17 09:21:14 +0900

    📑 :: MessgaeForm

---
+++ fcm-server/src/main/java/com/example/fcmserver/dto/MessageForm.java
@@ -0,0 +1,15 @@
+package com.example.fcmserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageForm {
+    private String message;
+    private String sender;
+}
