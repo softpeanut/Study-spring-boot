5f3d3d68e016b2eeee90d6f22b150d75678158dc
KimBeomJin <sdpthf@naver.com>
2022-04-04 17:22:18 +0900

    ⚙️ :: Init Project

---
+++ secondary-cache/src/main/java/com/example/secondarycache/SecondaryCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondarycache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondaryCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondaryCacheApplication.class, args);
+    }
+
+}
---
+++ secondary-cache/src/test/java/com/example/secondarycache/SecondaryCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondarycache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondaryCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
