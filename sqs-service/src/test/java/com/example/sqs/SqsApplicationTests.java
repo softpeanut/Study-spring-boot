0b9ebefec97b005ba2ee8717cdc007ed1f5f1ced
KimBeomJin <sdpthf@naver.com>
2022-04-22 10:50:34 +0900

    🌟 :: Init Project

---
+++ sqs-service/src/test/java/com/example/sqs/SqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
