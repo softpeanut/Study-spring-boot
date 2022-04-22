60cb1c72ea9cd722d31869809b9f7a2ff9003744
KimBeomJin <sdpthf@naver.com>
2022-04-22 22:06:08 +0900

    🌱 :: MessageController

---
+++ sqs-service/src/main/java/com/example/sqs/MessageController.java
@@ -0,0 +1,22 @@
+package com.example.sqs;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class MessageController {
+
+    private final AwsSQSSender awsSQSSender;
+
+    @PostMapping("/message")
+    public void send(@RequestBody String message) {
+        awsSQSSender.sendMessage(message);
+        log.info("send to sqs : " + message + ", " + System.currentTimeMillis());
+    }
+
+}
