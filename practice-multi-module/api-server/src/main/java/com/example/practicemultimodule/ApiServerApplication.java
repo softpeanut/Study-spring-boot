0a7fa192bf35be876fba2fce45f95ee4de22b257
KimBeomJin <sdpthf@naver.com>
2022-06-22 10:14:36 +0900

    📑 :: 멀티모듈 프로젝트

---
+++ practice-multi-module/api-server/src/main/java/com/example/practicemultimodule/ApiServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.practicemultimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServerApplication.class, args);
+    }
+}
---
+++ practice-multi-module/batch-server/src/main/java/com/example/practicemultimodule/BatchServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.practicemultimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchServerApplication.class, args);
+    }
+}
