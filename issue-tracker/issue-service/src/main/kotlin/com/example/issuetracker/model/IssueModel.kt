a2f9fd3e479e303b526bc87bf301a7a42ab63628
KimBeomJin <sdpthf@naver.com>
2022-08-02 15:30:16 +0900

    📑 :: IssueModel

---
+++ issue-tracker/issue-service/src/main/kotlin/com/example/issuetracker/model/IssueModel.kt
@@ -0,0 +1,49 @@
+package com.example.issuetracker.model
+
+import com.example.issuetracker.domain.Issue
+import com.example.issuetracker.domain.enums.IssuePriority
+import com.example.issuetracker.domain.enums.IssueStatus
+import com.example.issuetracker.domain.enums.IssueType
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.time.LocalDateTime
+
+data class IssueRequest(
+    val summary: String,
+    val description: String,
+    val type: IssueType,
+    val priority: IssuePriority,
+    val status: IssueStatus,
+)
+
+data class IssueResponse(
+    val id: Long,
+    val summary: String,
+    val description: String,
+    val userId: Long,
+    val type: IssueType,
+    val priority: IssuePriority,
+    val status: IssueStatus,
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val createdAt: LocalDateTime?,
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val updatedAt: LocalDateTime?,
+) {
+    companion object {
+        operator fun invoke(issue: Issue) =
+            with(issue) {
+                IssueResponse(
+                    id = id!!,
+                    summary = summary,
+                    description = description,
+                    userId = userId,
+                    type = type,
+                    priority = priority,
+                    status = status,
+                    createdAt = createdAt,
+                    updatedAt = updatedAt
+                )
+            }
+    }
+}
